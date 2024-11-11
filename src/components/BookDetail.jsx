136311d9642c834b94eb588b3a8a8769aafe1346
Henry Huynh <huynh.vu2201@gmail.com>
2024-11-11 14:42:05 -0600

    add Login and style to the page

--- src/components/BookDetail.jsx
+++ src/components/BookDetail.jsx
@@ -1,25 +1,28 @@
+// BookDetail.jsx
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getBookDetails } from "../api/api";
 
 const BookDetails = () => {
-  const {id} = useParams();
-  const [book, setBook] = useState([])
-  useEffect (() => {
-    const fetchBook = async () =>{
+  const { id } = useParams();
+  const [book, setBook] = useState([]);
+
+  useEffect(() => {
+    const fetchBook = async () => {
       const response = await getBookDetails(id);
       setBook(response.book);
     };
     fetchBook();
-  },[id])
-  
+  }, [id]);
+
   return (
-    <>
-    <h2>{book.title}</h2>
-    <p>{book.description}</p>
-    <p>Author: {book.author}</p>
-    <img src={book.coverimage} alt={book.title} height='500px' width='500px'/>
-  </>
-)
-}
-export default BookDetails
+    <div className="book-details-container">
+      <h2>{book.title}</h2>
+      <p>{book.description}</p>
+      <p>Author: {book.author}</p>
+      <img src={book.coverimage} alt={book.title} />
+    </div>
+  );
+};
+
+export default BookDetails;
