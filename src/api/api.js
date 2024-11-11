b523283ab24e8ed7e2e6c56c41a26f8904980d09
Henry Huynh <huynh.vu2201@gmail.com>
2024-11-10 22:09:45 -0600

    add register.jsx.

--- src/api/api.js
+++ src/api/api.js
@@ -13,3 +13,23 @@ export const getBookDetails = async (bookId) => {
   const idJson = await response.json();
   return idJson;
 };
+
+export const registerUser = async (userData) => {
+  const response = await fetch(`${APIURL}/users/register`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(userData),
+  });
+  const registerJson = await response.json();
+  return registerJson;
+};
+
+export const loginUser = async (credentials) => {
+  const response = await fetch(`${APIURL}/users/login`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(credentials),
+  });
+  const loginJson = await response.json();
+  return loginJson;
+};
